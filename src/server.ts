0a6089c656cd192f5578fd5464874aef3762a9fd
alirezanqp <alirezanqp@gmail.com>
2023-03-07 15:34:47 +0330

    add fastify server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
+import fastifyEnv from "@fastify/env";
 import Fastify from "fastify";
+import { options } from "./utils/config";
+
+export const server = Fastify()
+
+/*declare module 'fastify' {
+    interface FastifyInstance {
+      config: {
+        confKey: 'config',
+        PORT: number
+      };
+    }
+}*/
 
 function buildServer() {
-    const server = Fastify()
-}
+    // configs
+    // server.register(fastifyEnv, options).ready()
 
+    return server;
+}
 export default buildServer;
