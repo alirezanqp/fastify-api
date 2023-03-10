cc9cc6f754a81621256b8c552a2eafb1a7243372
alirezanqp <alirezanqp@gmail.com>
2023-03-10 23:20:04 +0330

    code foramt

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import buildServer from "./server"
+import buildServer from "./server";
 
 const server = buildServer();
-const port = 5000;
+const port = 8000;
 
 const start = async () => {
-    try {
-        await server.listen({ port })
+  try {
+    await server.listen({ port });
 
-        console.log(`Server ready at http://localhost:${port}`);
-    } catch (err) {
-        server.log.error(err)
-        process.exit(1)
-    }
-}
+    console.log(`Server ready at http://localhost:${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
 start();
